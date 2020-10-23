79bda6d4bfe1cc7680fb73ea63bd82b06efc1975
dlx996 <72247994+dlx996@users.noreply.github.com>
2020-10-23 07:16:19 +0530

    Perfomance update, Error Fixed, Comments Added

--- ex lab6 q1.py
+++ ex lab6 q1.py
@@ -1,30 +1,20 @@
 A = []
-
 for v in range(10):
     A.append(int(input("Enter a number between 10 to 20 : ")))
-
-print("\n")
-print(A)
-print(len(A))
+    
+print("Original Array : ", A)
+print("Length of Array : ", len(A))
 
 #insertion Sort
-
 def insertionSort(A):
-
     for j in range(1,len(A)):
-
         key = A[j]
         i = j - 1
-
         while (i >= 0 and A[i] > key):
-
             A[i + 1] = A[i]
-
             i = i - 1
-
         A[i + 1] = key
 
+#Call the funtion
 insertionSort(A)
-
-print("\nSorted Array\n")
-print(A)
+print("Sorted Array : ", A)
